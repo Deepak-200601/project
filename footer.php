bde08d5ffbbfce1ff9570dee6601393a08b99af9
Deepak-200601 <deepakdevadiga9591@gmail.com>
2024-01-25 10:11:40 +0530

    first version

---
+++ footer.php
@@ -0,0 +1,28 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="contact.php">contact</a>
+            <a href="shop.php">shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="orders.php">my orders</a>
+            <a href="cart.php">my cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +91 7022195214 </p>
+            <p> <i class="fas fa-envelope"></i> deepakdevadiga9591@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Bangalore, India </p>
+        </div>
+
+</section>
